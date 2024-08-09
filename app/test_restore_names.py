d4be942e10e9d9c93003f0e7c2797f74356b7d78
Misha Havryliuk <misharob31415@gmail.com>
2024-08-09 17:51:18 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,87 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+class TestRestore:
+    @pytest.mark.parametrize(
+        "income, result",
+        [
+            (
+                [{"last_name": "Holy", "full_name": "Jack Holy"}],
+                [
+                    {
+                        "first_name": "Jack",
+                        "last_name": "Holy",
+                        "full_name": "Jack Holy"
+                    }
+                ],
+            ),
+            (
+                [
+                    {
+                        "first_name": None,
+                        "last_name": "Holy",
+                        "full_name": "Jack Holy"
+                    }
+                ],
+                [
+                    {
+                        "first_name": "Jack",
+                        "last_name": "Holy",
+                        "full_name": "Jack Holy"
+                    }
+                ],
+            ),
+            (
+                [
+                    {
+                        "first_name": "Jack",
+                        "last_name": "Holy",
+                        "full_name": "Jack Holy"
+                    }
+                ],
+                [
+                    {
+                        "first_name": "Jack",
+                        "last_name": "Holy",
+                        "full_name": "Jack Holy"
+                    }
+                ],
+            ),
+            (
+                [
+                    {
+                        "first_name": None,
+                        "last_name": "Adams",
+                        "full_name": "Mike Adams"
+                    }
+                ],
+                [
+                    {
+                        "first_name": "Mike",
+                        "last_name": "Adams",
+                        "full_name": "Mike Adams"
+                    }
+                ],
+            ),
+            (
+                [
+                    {
+                        "first_name": "Mike",
+                        "last_name": "Adams",
+                        "full_name": "Mike Adams"
+                    }
+                ],
+                [
+                    {
+                        "first_name": "Mike",
+                        "last_name": "Adams",
+                        "full_name": "Mike Adams"
+                    }
+                ],
+            ),
+        ]
+    )
+    def test_restore(self, income: list[dict], result: list[dict]) -> None:
+        restore_names(income)
+        assert income == result
